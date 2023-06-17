d4c6313a606abf88f9897d74739dbd58245dd046
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-06-17 15:22:47 +0300

    corrected by comments

--- Homework_1/task_2.py
+++ Homework_1/task_2.py
@@ -4,12 +4,15 @@
 # то треугольника с такими сторонами не существует.
 # Отдельно сообщить является ли треугольник разносторонним, равнобедренным или равносторонним.
 
-side_a, side_b, side_c = int(input("Triangle side A:")), int(input("Triangle side B:")), int(input("Triangle side C:"))
+side_a, side_b, side_c = int(input("Triangle side A:")), \
+                         int(input("Triangle side B:")), \
+                         int(input("Triangle side C:"))
 message = None
 
 if side_a > side_b + side_c \
         or side_b > side_a + side_c \
-        or side_c > side_a + side_b:
+        or side_c > side_a + side_b \
+        or side_a == side_b == side_c == 0:
     message = "\nthere is no such triangle"
 else:
     if side_a != side_b != side_c:
